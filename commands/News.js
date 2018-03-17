e2d88f0043ed120d7f69213a31aad658cf179daa
71ck5 <32824732+71ck5@users.noreply.github.com>
2018-03-16 20:28:15 -0500

    Updated README / news command

--- commands/News.js
+++ commands/News.js
@@ -7,13 +7,30 @@ exports.run = (client, message, args) => {
       });
       res.on('end', function(){
         var Don = JSON.parse(body);
+		if (agrs[1] === "f") {
           do{
           console.log((num), Don.data.children[num].data.title);
           message.channel.send((num) + " " + Don.data.children[num].data.title);
           num++;
-        
-        while (num < args[1])
-      }
+		  }
+        while (num < args[2])
+      }else if (agrs[1] === "s") {
+		  do{
+          console.log(Don.data.children[num].data.url);
+          message.channel.send(Don.data.children[num].data.url);
+          num++;
+		  }
+        while (num < args[2])
+	  }else if (agrs[1] === "r") {
+		  do{
+		  console.log("www.reddit.com" + Don.data.children[num].data.permalink)
+          message.channel.send("www.reddit.com" + Don.data.children[num].data.permalink);
+          num++;
+		  }
+        while (num < args[2])
+	  }else {
+		  message.channel.send("$news <subreddit> f|s|r <number>")
+	  }
       });
   });
 }
