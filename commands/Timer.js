0a1229bcc1a170698497a59000a896833459073f
71ck5 <32824732+71ck5@users.noreply.github.com>
2018-04-06 18:45:54 -0500

    Bug fix
    It now refreshes the cache so that it gets new stories.

--- commands/Timer.js
+++ commands/Timer.js
@@ -1,15 +1,15 @@
 var ar = []
 exports.run = (client, message, args) => {
   const http = require('https');
-    http.get(`https://www.reddit.com/r/${args[0]}/top/.json`, (res) => {
-          var body = '';
-      res.on('data', function(chunk){
-          body += chunk;
-        });
-        res.on('end', function(){
-         var Don = JSON.parse(body);
          if( args[1] * 1000) {
              function news() {
+             http.get(`https://www.reddit.com/r/${args[0]}/top/.json`, (res) => {
+             var body = '';
+             res.on('data', function(chunk){
+             body += chunk;
+             });
+             res.on('end', function(){
+             var Don = JSON.parse(body);
              var m = 0
              while (m < Don.data.children.length && ar.indexOf(Don.data.children[m].data.url) != -1) {
               m++
@@ -22,12 +22,12 @@ exports.run = (client, message, args) => {
              message.channel.send(Don.data.children[m].data.url);
              ar.push(Don.data.children[m].data.url);
             }
-          }
+          })
+        })
+      }
           message.channel.send(`Timer Started for ${args[1]} hours, on the ${args[0]} subreddit`);
           setInterval(news, args[1] * 3600000);
           } else {
 			message.channel.send("Timer <subreddit> <Time in hours>")
       }
-  })
-})
-}
+  }
